da7bc521bc5cce2a01224d945ec861b1500d9aac
kevinyuchi <yuchi.kevin@gmail.copm>
2017-07-02 15:02:32 -0700

    Initial commit

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,17 @@
+package controller;
+
+/**
+ * Created by kkaiwen on 7/2/17.
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+@RequestMapping("/")
+public String index() {
+	return "Greetings from Spring Boot!\n";
+}
+
+}
